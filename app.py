978ee8998532cc63baa7932be1255dec78236e2b
Aatif Zaidi <aatif@example.com>
2025-07-14 03:38:49 -0500

    Resolved merge conflicts and finalized dashboard

--- app.py
+++ app.py
@@ -297,4 +297,3 @@ else:
 |                     |                |               |                  |
 | **➡️ Overall Confidence** |       —        |       —       | **{overall_confidence}/100** |
 """)
-
